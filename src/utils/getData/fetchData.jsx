2f54b57c9e9a8ad8a3797bdbbf855b7341d61469
Nick Essing <nick.essing@tobit.software>
2018-08-29 17:54:57 +0200

    create tempList /beginn fetchData

--- src/utils/getData/fetchData.jsx
+++ src/utils/getData/fetchData.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 export default (link) => {
-        return new Promise((resolve, reject) => {
-            fetch(link)
-                .then((response) => {
-                    return (response.json());
-                }).then((data) => {
-                    console.log('parsed json', data)
-                    resolve(data);
-                }).catch((ex) => {
-                    console.log('parsing failed', ex);
-                    reject(ex);
-                });
-        }); 
-    }
+    return new Promise((resolve, reject) => {
+        fetch(link)
+            .then((response) => {
+                return (response.json());
+            }).then((data) => {
+                console.log('parsed json', data)
+                resolve(data);
+            }).catch((ex) => {
+                console.log('parsing failed', ex);
+                reject(ex);
+            });
+    }); 
+}
