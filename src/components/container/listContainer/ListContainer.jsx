88e1e186be458b6fe664e2be10ff5999279258d7
Nick Essing <nick.essing@tobit.software>
2018-08-30 17:49:50 +0200

    Bug fixes/error catches

--- src/components/container/listContainer/ListContainer.jsx
+++ src/components/container/listContainer/ListContainer.jsx
@@ -25,14 +25,19 @@ export default class ListContainer extends React.Component {
         );
     }
     appendList = (searchString) => {
-        fetchData("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=" + searchString + "&Skip="+ this.state.tempList.length +"&Take="+ this.state.count).then((data) => {  
-            console.log(this.state.tempList.length);
-        if (data.Data !== null){
-                this.setState({
-                    tempList: this.state.tempList.concat(data.Data),
-                    searchString
-                });
-            }
-        });
+        if(this.state.tempList !== null){
+            fetchData("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=" + searchString + "&Skip="+ this.state.tempList.length +"&Take="+ this.state.count).then((data) => {    
+                    if (data.Data !== null){
+                        this.setState({
+                            tempList: this.state.tempList.concat(data.Data),
+                            searchString
+                        });
+                    }else{
+                        this.setState({
+                            tempList: null
+                        });           
+                    }
+            });
+        }
     } 
 }
--- src/components/container/listContainer/createList/CreateList.jsx
+++ src/components/container/listContainer/createList/CreateList.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './listSyles.scss'
 import ListItem from './ListItem';
+import PropTypes from 'prop-types';
 
-export default class CreateList extends React.Component {
+const propTypes ={
+    tempList: PropTypes.array
+};
+const defaultProps ={
+    tempList: []
+}
+
+class CreateList extends React.Component {
 
     render(){
         return (
         <React.Fragment>
-            {this.props.tempList.map(s => <ListItem key={s.siteId} {...s} />)}
+            {this.props.tempList !== null ? this.props.tempList.map((s) => <ListItem key={s.siteId} {...s} />): <div style={{textAlign: 'center'}}><br /><h3>Keine Ergebnisse gefunden</h3></div>}
         </React.Fragment>
         );
     } 
 }
+
+CreateList.propTypes = propTypes;
+
+export default CreateList;
