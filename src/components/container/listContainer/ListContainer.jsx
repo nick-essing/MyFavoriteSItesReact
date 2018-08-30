edc548151049cd2f5eb7d8378f273c95200f8049
Nick Essing <nick.essing@tobit.software>
2018-08-30 13:58:23 +0200

    fixed moreBtn bug

--- src/components/container/listContainer/ListContainer.jsx
+++ src/components/container/listContainer/ListContainer.jsx
@@ -25,12 +25,14 @@ export default class ListContainer extends React.Component {
         );
     }
     appendList = (searchString) => {
-        fetchData("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=" + searchString + "&Skip="+ this.state.tempList.length +"&Take="+ this.state.count).then((data) => {
-            this.setState({
-                tempList: this.state.tempList.concat(data.Data),
-                searchString
-            });
+        fetchData("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=" + searchString + "&Skip="+ this.state.tempList.length +"&Take="+ this.state.count).then((data) => {  
+            console.log(this.state.tempList.length);
+        if (data.Data !== null){
+                this.setState({
+                    tempList: this.state.tempList.concat(data.Data),
+                    searchString
+                });
+            }
         });
     } 
 }
-
